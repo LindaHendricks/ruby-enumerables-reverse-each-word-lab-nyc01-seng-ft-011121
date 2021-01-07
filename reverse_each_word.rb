e6b77137c2633f7347c3acd5838659914e29cce7
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2021-01-07 21:52:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word (string)
   string_to_array = string.split(" ")
   new_reverse_array = [ ]
-  new_reverse_array2 = [ ]
   
   string_to_array.each do |element|
     element.reverse 
