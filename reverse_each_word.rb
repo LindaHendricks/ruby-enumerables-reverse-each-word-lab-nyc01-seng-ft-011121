4f301730a806daedfce5d50db47f74ded2ee8b73
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2021-01-07 22:00:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,3 +12,18 @@ end
 new_reverse_array.join(" ")
 end
 
+
+def reverse_each_word (string)
+  string_to_array = string.split(" ")
+  new_reverse_array = [ ]
+  
+  string_to_array.collect do |element|
+    element = element.reverse 
+    new_reverse_array << element
+  
+  #shall I create a new loop where I reverse each element here ?? I try with .reverse but I don't succeed.
+  
+end 
+new_reverse_array.join(" ")
+end
+
